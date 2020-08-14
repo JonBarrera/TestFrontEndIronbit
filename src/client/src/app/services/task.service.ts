28239e3d4e982a39197162243036c27e8810faeb
JonBarrera <69278604+JonBarrera@users.noreply.github.com>
2020-08-14 11:48:31 -0500

    Se corrigio rutas de la api en client

--- src/client/src/app/services/task.service.ts
+++ src/client/src/app/services/task.service.ts
@@ -8,25 +8,23 @@ import { Task } from '../Task';
   providedIn: 'root'
 })
 export class TaskService {
-  
-  domain: string ='test-front-end-ironbit.vercel.app/';
 
   constructor(private http: HttpClient) { }
 
   //to Get task
   getTasks(){
-  return this.http.get<Task[]>(`${this.domain}/api/tasks`)
+  return this.http.get<Task[]>('/api/tasks')
     .pipe(map(res => res));
 
   }
   //to Add task
   addTask(newTask: Task){
-  return this.http.post<Task>(`${this.domain}/api/tasks`, newTask)
+  return this.http.post<Task>('/api/tasks', newTask)
     .pipe(map(res => res));
   }
   //to Delete task
   deleteTask(id) {
-    return this.http.delete<Task>(`${this.domain}/api/tasks/${id}`)
+    return this.http.delete<Task>(`/api/tasks/${id}`)
       .pipe(map(res => res));
   }
 }
