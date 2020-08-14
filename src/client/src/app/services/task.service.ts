84dc0746986a9af49b1919866f8460e971f96c26
JonBarrera <69278604+JonBarrera@users.noreply.github.com>
2020-08-14 11:14:31 -0500

    Se reparo el domain y se integro le domino vercel

--- src/client/src/app/services/task.service.ts
+++ src/client/src/app/services/task.service.ts
@@ -3,27 +3,30 @@ import { HttpClient } from "@angular/common/http";
 import {map} from "rxjs/operators/map";
 import { Task } from '../Task';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  
+  domain: string ='test-front-end-ironbit.vercel.app/';
 
   constructor(private http: HttpClient) { }
 
   //to Get task
   getTasks(){
-  return this.http.get<Task[]>(`/api/tasks`)
+  return this.http.get<Task[]>(`${this.domain}/api/tasks`)
     .pipe(map(res => res));
 
   }
   //to Add task
   addTask(newTask: Task){
-  return this.http.post<Task>(`/api/tasks`, newTask)
+  return this.http.post<Task>(`${this.domain}/api/tasks`, newTask)
     .pipe(map(res => res));
   }
   //to Delete task
   deleteTask(id) {
-    return this.http.delete<Task>(`/api/tasks/${id}`)
+    return this.http.delete<Task>(`${this.domain}/api/tasks/${id}`)
       .pipe(map(res => res));
   }
 }
